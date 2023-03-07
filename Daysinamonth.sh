f87f1578742dbdc9a8aafbf8c1acc47e9d7ae244
Raneen AK <raneen.ak@gmail.com>
2023-03-07 02:33:28 -0800

    these are my changes

--- Daysinamonth.sh
+++ Daysinamonth.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
-check_months_daysum(){
-case $1 in 
-2) return 28 ;;
-4|6|9|11) return 30 ;;
-1|3|5|7|8|12) return 31 ;;
+source 3f.sh
+
+check_months_daysum(){ 
+case $2 in 
+2) 
+if is_leap $1; 
+then 
+echo "29" 
+else echo "28" 
+fi 
+;; 
+4|6|9|11) return 30 ;; 
+1|3|5|7|8|12) return 31 ;; 
 esac
 }
-check_months_daysum $1
+check_months_daysum $2 $1
