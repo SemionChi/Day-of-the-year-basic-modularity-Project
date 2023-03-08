daaa882b4526340cfc61de288fb2174d99e364e1
Raneen AK <raneen.ak@gmail.com>
2023-03-08 11:26:29 +0200

    this is the final project

--- main.sh
+++ main.sh
@@ -7,23 +7,24 @@ source inputval.sh #Input validation
 
 read -p "Please enter a date by this format: Year Month Day : " year month day
 if  validate $year $month $day ;then
-	read -p "Select func num 1 2 3 : " selected
-	case $selected in 
+	echo "please select your desired option"
+        function=("Days till entered date" "How many days per month" "Is it leap year?" "Exit")
+        select i in "${function[@]}" ; do
+	case $i in 
 
-		1)
-			get_days_num $year $month $day
-			;;
-		2)
-			check_months_daysum $year $month
-			;;
-		3)
+		"Days till entered date") get_days_num $year $month $day ;;
+		"How many days per month") check_months_daysum $year $month ;;
+		"Is it leap year?")
 			if is_leap $year; then
 				echo "$year is a leap year"
 			else
 				echo "$year is not a leap year"
 			fi
 			;;
+		"Exit") echo "You are leaving too soon :( bye" 
+		exit ;;
 	esac
+	done
 else
 	echo "Invalid date values"
 fi
